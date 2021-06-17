a3b4c4d0d17ce00fad6d8cc2bca686def7003564
timilxr <timilxr@gmail.com>
2021-06-17 19:38:46 +0100

    First commit on React-redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,29 @@
+import { ActionTypes } from "../constants/action-types"
+
+const initialState = {
+    products: []
+}
+export const productReducer = (state = initialState, {type, payload}) => {
+    switch(type){
+        case ActionTypes.SET_PRODUCTS:
+            return {...state, products: payload};
+        break;
+        default:
+            return state;
+        break;
+    }
+}
+
+export const selectedProductReducer = (state = {}, {type, payload}) => {
+    switch(type){
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+        break;
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
+        break;
+        default:
+            return state;
+        break;
+    }
+}
